98957c13756a09f78afc9659c3dc87f3d438f95b
awangelo <anggg.gaio@gmail.com>
2024-07-13 18:50:40 -0300

    548

--- struct-aluno-livro.c
+++ struct-aluno-livro.c
@@ -11,56 +11,151 @@ typedef struct {
     float preco;
 } Livro;
 
-Livro *create_livro(const char *titulo, unsigned int paginas, float preco);
-void delete_livro(Livro *livro);
-void print_livro(Livro *livro);
+typedef struct {
+    char nome[BUFFER_SIZE];
+    unsigned short idade;
+    Livro *livro;
+} Aluno;
+
+Livro *create_livro(const char *p_titulo, unsigned int paginas, float preco);
+Livro *copy_livro(const Livro *p_livro);
+void delete_livro(Livro **pp_livro);
+void print_livro(const Livro *p_livro);
+Aluno *create_aluno(const char *p_nome, unsigned short idade, const Livro *p_livro);
+void delete_aluno(Aluno **pp_aluno);
+void print_aluno(const Aluno *p_aluno);
+void clear_input_buffer();
 
 int
 main()
 {
-    printf("%s", "Digite o titulo do livro: ");
     char titulo[BUFFER_SIZE];
+    unsigned int paginas;
+    float preco;
+    char nome[BUFFER_SIZE];
+    unsigned short idade;
+
+    printf("%s", "Digite o nome do aluno: ");
+    fgets(nome, BUFFER_SIZE, stdin);
+    nome[strcspn(nome, "\n")] = '\0';
+
+    printf("%s", "Digite a idade do aluno: ");
+    while (scanf("%3hu", &idade) != 1) {
+        clear_input_buffer();
+        printf("Digite uma idade valida: ");
+    }
+    clear_input_buffer();
+
+    printf("%s", "Digite o titulo do livro: ");
     fgets(titulo, BUFFER_SIZE, stdin);
     titulo[strcspn(titulo, "\n")] = '\0';
-    printf("%s", "Digite o numero de paginas: ");
-    unsigned int paginas;
-    scanf("%5u", &paginas);
-    printf("%s", "Digite o preco do livro: ");
-    float preco;
-    scanf("%10f", &preco);
 
-    Livro *awawawa = create_livro(titulo, paginas, preco);
-    print_livro(awawawa);
-    puts("\nDeletando livro...");
-    delete_livro(awawawa);
+    printf("%s", "Digite o numero de paginas: ");
+    while (scanf("%5u", &paginas) != 1) {
+        clear_input_buffer();
+        printf("Digite um numero valido de páginas: ");
+    }
+    clear_input_buffer();
+
+    printf("%s", "Digite o preco do livro: ");
+    while (scanf("%10f", &preco) != 1) {
+        clear_input_buffer();
+        printf("Digite um preco valido: ");
+    }
+    clear_input_buffer();
+
+    Livro *p_livro = create_livro(titulo, paginas, preco);
+    Aluno *p_aluno = create_aluno(nome, idade, p_livro);
+
+    print_aluno(p_aluno);
+    puts("\nDeletando aluno...");
+    delete_aluno(&p_aluno); // Deletando o aluno, copia do livro
+
+    delete_livro(&p_livro); // Deletando o livro original (ultimo erro no valgrind)
 }
 
 Livro *
-create_livro(const char *titulo, unsigned int paginas, float preco)
+create_livro(const char *p_titulo, unsigned int paginas, float preco)
 {
-    Livro *new_livro = malloc(sizeof(Livro));
-    if (new_livro == NULL) {
+    Livro *p_new_livro = malloc(sizeof(Livro));
+    if (p_new_livro == NULL) {
         fprintf(stderr, "Erro ao criar livro: %s\n", strerror(errno));
         exit(EXIT_FAILURE);
     }
 
-    strncpy(new_livro->titulo, titulo, BUFFER_SIZE);
-    new_livro->paginas = paginas;
-    new_livro->preco   = preco;
+    strncpy(p_new_livro->titulo, p_titulo, BUFFER_SIZE);
+    p_new_livro->titulo[BUFFER_SIZE - 1] = '\0';
 
-    return new_livro;
+    p_new_livro->paginas = paginas;
+    p_new_livro->preco   = preco;
+
+    return p_new_livro;
+}
+
+Livro *
+copy_livro(const Livro *p_livro)
+{
+    return create_livro(p_livro->titulo, p_livro->paginas, p_livro->preco);
 }
 
 void
-delete_livro(Livro *livro)
+delete_livro(Livro **pp_livro)
 {
-    free(livro);
+    if (pp_livro != NULL && *pp_livro != NULL) {
+        free(*pp_livro);
+        *pp_livro = NULL;
+    }
 }
 
 void
-print_livro(Livro *livro)
+print_livro(const Livro *p_livro)
 {
-    printf("Titulo: %5s\n", livro->titulo); // TODO: FORMATAR DIREITO
-    printf("Paginas: %3u\n", livro->paginas);
-    printf("Preco: %8.2f\n", livro->preco);
+    printf("Titulo: %s\n", p_livro->titulo);
+    printf("Paginas: %u\n", p_livro->paginas);
+    printf("Preco: %.2f\n", p_livro->preco);
+}
+
+Aluno *
+create_aluno(const char *p_nome, unsigned short idade, const Livro *p_livro)
+{
+    Aluno *p_new_aluno = malloc(sizeof(Aluno));
+    if (p_new_aluno == NULL) {
+        fprintf(stderr, "Erro ao criar aluno: %s\n", strerror(errno));
+        exit(EXIT_FAILURE);
+    }
+
+    strncpy(p_new_aluno->nome, p_nome, BUFFER_SIZE);
+    p_new_aluno->nome[BUFFER_SIZE - 1] = '\0';
+
+    p_new_aluno->idade = idade;
+    p_new_aluno->livro = copy_livro(p_livro);
+
+    return p_new_aluno;
+}
+
+void
+delete_aluno(Aluno **pp_aluno)
+{
+    if (pp_aluno != NULL && *pp_aluno != NULL) {
+        delete_livro(&(*pp_aluno)->livro);
+        free(*pp_aluno);
+        *pp_aluno = NULL;
+    }
+}
+
+void
+print_aluno(const Aluno *p_aluno)
+{
+    printf("Nome: %s\n", p_aluno->nome);
+    printf("Idade: %hu\n", p_aluno->idade);
+    puts("Livro do aluno:");
+    print_livro(p_aluno->livro);
+}
+
+void
+clear_input_buffer()
+{
+    int c;
+    while ((c = getchar()) != '\n' && c != EOF) {
+    }
 }
